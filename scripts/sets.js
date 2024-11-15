be7ef51946c447edf1867b3747ee3fe5473ae592
Bjorn Van Acker <bjorn.vanacker@team.blue>
2024-11-15 15:14:06 +0100

    Setup proxy generator for custom sets

---
+++ scripts/sets.js
@@ -0,0 +1,116 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    const setsSelect = document.getElementById("sets");
+    const showEnchanted = document.getElementById("showEnchanted");
+    const showPromo = document.getElementById("showPromo");
+    const cardsContainer = document.getElementById("cardsContainer");
+    const printButton = document.getElementById("printButton");
+
+    const getSets = async () => {
+        const response = await fetch("https://api.lorcast.com/v0/sets");
+
+        return await response.json();
+    };
+
+    const addSetsToSelect = async () => {
+        const sets = await getSets();
+
+        sets.results.forEach((set) => {
+            const optionElement = document.createElement("option");
+            optionElement.value = set.code;
+            optionElement.textContent = set.name;
+
+            setsSelect.appendChild(optionElement);
+        });
+
+        // Select the first set by default
+        setsSelect.selectedIndex = 1;
+        await renderSet(sets.results[0].code);
+
+        setsSelect.classList.remove("hidden");
+    };
+
+    const renderSet = async (code) => {
+        const response = await fetch(
+            `https://api.lorcast.com/v0/sets/${code}/cards`
+        );
+        const cards = await response.json();
+
+        let fullGrayscale = false;
+        if (code === "cp" || code === "D23") {
+            fullGrayscale = true;
+        }
+
+        const showEnchantedCards = showEnchanted.checked;
+        const showPromoCards = showPromo.checked;
+
+        cards.forEach((card) => {
+            if (!showEnchantedCards && card.rarity === "Enchanted") {
+                return;
+            }
+
+            if (!showPromoCards && card.rarity === "Promo") {
+                return;
+            }
+
+            const cardElement = document.createElement("div");
+            cardElement.classList.add("card");
+            if (card.type.includes("Location")) {
+                cardElement.classList.add("location");
+            }
+
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "x";
+            deleteButton.classList.add("delete-button");
+            deleteButton.classList.add("display-only");
+
+            const imgElement = document.createElement("img");
+            imgElement.src = card.image_uris.digital.large;
+
+            const grayscaleMask = document.createElement("div");
+            grayscaleMask.classList.add("grayscale-mask");
+
+            if (fullGrayscale || card.rarity === "Enchanted") {
+                grayscaleMask.classList.add("full-grayscale-mask");
+            }
+
+            const grayscaleImg = document.createElement("img");
+            grayscaleImg.src = card.image_uris.digital.large;
+
+            grayscaleMask.appendChild(grayscaleImg);
+            cardElement.appendChild(imgElement);
+            cardElement.appendChild(grayscaleMask);
+            cardElement.appendChild(deleteButton);
+            cardsContainer.appendChild(cardElement);
+
+            cardElement.addEventListener("click", () => {
+                // Remove the card from the container
+                cardElement.remove();
+            });
+        });
+    };
+
+    printButton.addEventListener("click", () => {
+        window.print();
+    });
+
+    const selectSet = async () => {
+        const selectedSet = setsSelect.value;
+
+        // Clear the cards container
+        cardsContainer.innerHTML = "";
+
+        if (selectedSet === "") {
+            return;
+        }
+
+        await renderSet(selectedSet);
+    };
+
+    showEnchanted.addEventListener("change", selectSet);
+
+    showPromo.addEventListener("change", selectSet);
+
+    setsSelect.addEventListener("change", selectSet);
+
+    await addSetsToSelect().then(() => selectSet());
+});
